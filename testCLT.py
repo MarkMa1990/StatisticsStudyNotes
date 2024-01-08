5e171e9e2015764469bbf49cf7a91a84823459b8
hongfeng <hm@nilt.com>
2024-01-08 16:32:21 +0100

    Central Limit Theorem test

---
+++ testCLT.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+Nexp = 10000
+
+x_avg_all = np.zeros(Nexp)
+for i0 in range(Nexp):
+    np.random.seed(np.random.randint(1,1e9,size=1))
+    
+    N = 10
+    #mu = 1
+    #sigma = 0.5
+    #x = np.random.normal(mu,sigma,N)
+    #np.random.seed(np.random.randint(1,1000,size=1))
+    x = np.random.random(N)
+    
+    mu_exp = np.mean(x)
+    #sigma_exp = np.std(x,ddof=1)
+    
+    x_avg_all[i0]=mu_exp
+
+x = np.float32(x_avg_all)
+
+#print(abs(mu - np.mean(x)),abs(sigma-np.std(x,ddof=1)))
+
+hist, edges = np.histogram(x,bins=100,)
+
+plt.figure()
+plt.bar(edges[0:-1],hist,align='center',width=1/500)
+
+plt.figure()
+plt.boxplot(x,
+    vert=True,  # vertical box alignment
+    patch_artist=False,  # fill with color
+    )
+plt.show()
